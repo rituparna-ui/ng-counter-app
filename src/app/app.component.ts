cae80a647b24032a0d8fbdfd86a11e7810d2ef60
Rituparna Warwatkar <rwarwatkar@gmail.com>
2020-12-11 23:22:31 +0530

    :tada: finished counter app project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+  count: number = 0;
+
+  increaseCount = () => {
+    this.count = this.count + 1;
+  };
+
+  decreaseCount = () => {
+    if (this.count === 0) return;
+    this.count = this.count - 1;
+  };
+
+  resetCount = () => {
+    this.count = 0;
+  };
 }
